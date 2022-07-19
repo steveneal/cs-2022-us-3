d7b4b90a4bf01e78bd42a02bdacc5a2302c7e781
ruotian-331 <ruotian@alumni.unc.edu>
2022-07-19 16:01:18 -0400

    added trades by entity and test

--- src/test/java/com/cs/rfq/decorator/extractors/TotalTradesWithEntityExtractorTest.java
+++ src/test/java/com/cs/rfq/decorator/extractors/TotalTradesWithEntityExtractorTest.java
@@ -1,2 +1,46 @@
-package com.cs.rfq.decorator.extractors;public class TotalTradesWithEntityExtractorTest {
+package com.cs.rfq.decorator.extractors;
+
+import com.cs.rfq.decorator.Rfq;
+import com.cs.rfq.decorator.TradeDataLoader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class TotalTradesWithEntityExtractorTest extends AbstractSparkUnitTest {
+    String string;
+    Rfq rfq;
+    Dataset<Row> data;
+    SparkSession session;
+
+    @BeforeEach public void setUp() {
+        string = "{" +
+                "'id': '123ABC', " +
+                "'traderId': 3351266293154445953, " +
+                "'entityId': 5561279226039690843, " +
+                "'instrumentId': 'AT0000383864', " +
+                "'qty': 250000, " +
+                "'price': 1.58, " +
+                "'side': 'B' " +
+                "}";
+
+        rfq = Rfq.fromJson(string);
+
+        String filePath = getClass().getResource("test-trades.json").getPath();
+        session = SparkSession.builder()
+                .appName("TotalTradesByEntitySession")
+                .master("local")
+                .getOrCreate();
+        data = new TradeDataLoader().loadTrades(session, filePath);
+    }
+
+    @Test
+    public void extractTotalTradesWithEntityByDate() {
+        TotalTradesWithEntityExtractor extractorByDay = new TotalTradesWithEntityExtractor();
+        Map<RfqMetadataFieldNames, Object> output = extractorByDay.extractMetaData(rfq,session,data);
+        System.out.println(output);
+    }
 }
